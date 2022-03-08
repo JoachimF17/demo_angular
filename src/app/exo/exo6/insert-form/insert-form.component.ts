70d3fa3450f455ac70ceb8ec2576817caa1a8ad5
Joachim <joachim.frances@hotmail.be>
2022-03-08 15:23:21 +0100

    exo formulaire

--- src/app/exo/exo6/insert-form/insert-form.component.ts
+++ src/app/exo/exo6/insert-form/insert-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { priceDoubleStock, PRODUIT_INSERT_FORM, sameTwoLetters } from 'src/app/models/forms/produit.form';
 import { Produit } from 'src/app/models/produit.model';
 
 @Component({
@@ -12,25 +13,26 @@ export class InsertFormComponent implements OnInit {
   @Output('produit-submitted')
   produitSubmitted = new EventEmitter<Produit>()
 
-  produitInsertForm = new FormGroup({
-    'marque': new FormControl(undefined, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]),
-    'modele': new FormControl(undefined, [Validators.minLength(1), Validators.maxLength(30)]),
-    'prix': new FormControl(0, [Validators.required, Validators.min(0)]),
-    'stock': new FormControl(1, [Validators.required, Validators.min(1), Validators.max(100)]),
-    'enVente': new FormControl(true),
-  });
+  produitInsertForm: FormGroup;
 
-  constructor() { }
+  constructor(builder: FormBuilder) {
+    this.produitInsertForm = builder.group(PRODUIT_INSERT_FORM, {
+      validators: [priceDoubleStock, sameTwoLetters]
+    });
+   }
 
   ngOnInit(): void {
   }
 
   onSubmit(){
-    if(this.produitInsertForm.get('stock')?.invalid)
-      this.produitInsertForm.get('stock')?.setValue(1)
-
     if(this.produitInsertForm.valid)
-      this.produitSubmitted.emit(this.produitInsertForm.value)
+      this.produitSubmitted.emit({
+        'marque': this.produitInsertForm.value.marque,
+        'modele': this.produitInsertForm.value.modele,
+        'prix': this.produitInsertForm.value.prix,
+        'stock': isNaN(this.produitInsertForm.value.stock) ? 1 : this.produitInsertForm.value.stock,
+        'enVente': this.produitInsertForm.value.enVente
+      })
   }
 
 }
---
+++ src/app/models/forms/produit.form.ts
@@ -0,0 +1,101 @@
+import { AbstractControl, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+export const PRODUIT_INSERT_FORM = {
+    'marque': [undefined, [Validators.required, Validators.maxLength(30), atLeastTwoLetters]],
+    'modele': [undefined, [Validators.minLength(1), Validators.maxLength(30)]],
+    'prix': [0, [Validators.required, Validators.min(0), testValidateur(3)]],
+    'stock': [1, [Validators.min(1), Validators.max(100), pair]],
+    'enVente': [true]
+  };
+
+  function pair(control: AbstractControl): ValidationErrors | null{
+    if(!control.value || control.value % 2 == 0)
+        return null;
+    
+    return{
+        pair: {
+            'message': 'doit etre pair'
+        }
+    }
+  }
+
+  function osef(param: number){
+    function pairInt(control: AbstractControl): ValidationErrors | null{
+        if(!control.value || control.value % param == 0)
+            return null;
+        
+        return{
+            pair: {
+                'message': 'doit etre pair'
+            }
+        }
+    }
+
+    return pairInt;
+  }
+
+  function atLeastTwoLetters(control: AbstractControl): ValidationErrors | null{
+    let marque = control.value;
+
+    if(typeof marque === 'string' && marque.length >= 2)
+        return null;
+
+    return{
+        atLeastTwoLetters: {
+            'marque': marque,
+            'message': 'au moins 2 caracteres'
+        }
+    }
+  }
+
+  export function sameTwoLetters(control: AbstractControl): ValidationErrors | null{
+    let marque = control.value.marque;
+    let modele = control.value.modele;
+
+    if(typeof marque === 'string' && typeof modele === 'string' && marque.length >= 2 
+    && modele.length >= 2 && marque.substring(0, 2) === modele.substring(0, 2))
+        return null;
+    
+    return{
+        sameTwoLetters: {
+            'marque': marque,
+            'modele': modele,
+            'message': 'le modele et la marque doivent commencer par les 2 memes lettres'
+        }
+    }
+  }
+
+  export function priceDoubleStock(control: AbstractControl): ValidationErrors | null{
+    let prix = control.value.prix;
+    let stock = control.value.stock;
+
+    if(prix >= 2*stock)
+        return null;
+    
+    return{
+        priceDoubleStock: {
+            'prix': prix,
+            'stock': stock,
+            'message': 'le prix doit etre le double du stock'
+        }
+    }
+  }
+
+  function validateurMultiple(param: number): ValidatorFn | null{
+    return (control: AbstractControl) => {
+        if(control.value % param === 0)
+            return null;
+        
+        return{
+            testValidateur:{
+                'parametre': param,
+                'valeur': control.value,
+                'message': 'doit etre un multiple de '+param
+            }
+        }
+    }
+  }
+
+  function testValidateur(div: number): ValidationErrors | null{
+    return validateurMultiple(div);
+  }
